e87b213f31121fbaabd6a7b6c87510fd6850ab7a
KajE879 <9024897@student.zadkine.nl>
2025-01-13 16:15:08 +0100

    made a first interface for the tamagothi

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>TAMAGOTCHI</title>
+</head>
+<body>
+    <div class="container">
+        <div class="screen">
+            <div class="stats-bar">
+                <div id="hunger">🍗100</div>
+                <div id="sleep">🛏️100</div>
+                <div id="fun">🛝100</div>
+            </div>
+            <div class="character">
+                <img src="images/kw.jpg" alt="A tamagotchi character of kanye west">
+            </div>
+        </div>
+        <div class="buttons">
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+        <h1>TAMAGOTCHI ©</h1>
+    </div>
+    <script src="main.js"></script>
+</body>
+</html>
